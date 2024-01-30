772dfecfea000c5a763e732fe7c929680d8a1cd5
Caio Teixeira <caio.teixeira@unyleya.com.br>
2024-01-30 11:47:01 -0300

    fix: remove no-used types

--- src/types.ts
+++ src/types.ts
@@ -2,14 +2,6 @@ import type { Properties } from 'csstype';
 
 export type Falsy = undefined | null | false | 0 | -0 | 0n | '';
 
-export type RecursivePartial<T> = {
-  [P in keyof T]?: T[P] extends (infer U)[]
-    ? RecursivePartial<U>[]
-    : T[P] extends object | undefined
-      ? RecursivePartial<T[P]>
-      : T[P];
-};
-
 export type Parser = (value: any, prop: string) => ComettaStyle | Falsy;
 
 export type Alias = {
