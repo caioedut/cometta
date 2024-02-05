83d15d0ded24a341b5b28c2b75524be187688e3e
Caio Teixeira <caio.teixeira@unyleya.com.br>
2024-02-05 17:08:36 -0300

    build: update script

--- scripts/publish.ts
+++ scripts/publish.ts
@@ -8,9 +8,6 @@ pmex('build');
 
 pmex('npm version patch');
 
-copyFileSync('package.json', 'dist/package.json');
-copyFileSync('README.md', 'dist/README.md');
-
 execSync('npm publish', { stdio: 'inherit', cwd: './dist' });
 
 execSync('git push', { stdio: 'inherit' });
