e7b49ed3cc0edc27dc6f0663450d8edfc7475c3a
Caio Teixeira <caio.teixeira@unyleya.com.br>
2025-10-19 15:09:18 -0300

    feat: add support for resolving 'outline' and multi-value 'padding'/'margin' properties

--- tests/prepare.test.ts
+++ tests/prepare.test.ts
@@ -22,6 +22,16 @@ describe('prepare()', () => {
     });
   });
 
+  test('must resolve prop outline', () => {
+    const style = prepare('outline: 1px solid red');
+
+    expect(style).toEqual({
+      outlineWidth: 1,
+      outlineColor: 'red',
+      outlineStyle: 'solid',
+    });
+  });
+
   test('must resolve prop border', () => {
     const style = prepare('border: 1px solid red');
 
@@ -40,6 +50,72 @@ describe('prepare()', () => {
       borderColor: 'red',
       borderStyle: 'solid',
       borderLeftWidth: 0,
+    });
+  });
+
+  test('must resolve 2-value margin to each prop', () => {
+    const style = prepare('margin: 10px 20px');
+
+    expect(style).toEqual({
+      marginTop: 10,
+      marginRight: 20,
+      marginBottom: 10,
+      marginLeft: 20,
+    });
+  });
+
+  test('must resolve 3-value margin to each prop', () => {
+    const style = prepare('margin: 10px 20px 30px');
+
+    expect(style).toEqual({
+      marginTop: 10,
+      marginRight: 20,
+      marginBottom: 30,
+      marginLeft: 20,
+    });
+  });
+
+  test('must resolve 4-value margin to each prop', () => {
+    const style = prepare('margin: 10px 20px 30px 40px');
+
+    expect(style).toEqual({
+      marginTop: 10,
+      marginRight: 20,
+      marginBottom: 30,
+      marginLeft: 40,
+    });
+  });
+
+  test('must resolve 2-value padding to each prop', () => {
+    const style = prepare('padding: 10px 20px');
+
+    expect(style).toEqual({
+      paddingTop: 10,
+      paddingRight: 20,
+      paddingBottom: 10,
+      paddingLeft: 20,
+    });
+  });
+
+  test('must resolve 3-value padding to each prop', () => {
+    const style = prepare('padding: 10px 20px 30px');
+
+    expect(style).toEqual({
+      paddingTop: 10,
+      paddingRight: 20,
+      paddingBottom: 30,
+      paddingLeft: 20,
+    });
+  });
+
+  test('must resolve 4-value padding to each prop', () => {
+    const style = prepare('padding: 10px 20px 30px 40px');
+
+    expect(style).toEqual({
+      paddingTop: 10,
+      paddingRight: 20,
+      paddingBottom: 30,
+      paddingLeft: 40,
     });
   });
 
